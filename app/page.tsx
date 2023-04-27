9c72a7ea0e53eee8c2342f09b23dd938eadce689
Aaron Hsieh <110440243+ahsiehgit@users.noreply.github.com>
2023-04-27 23:43:08 +0800

    index fix

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,8 @@ import MiniPage from "@/components/Works/MiniPage";
 export const dynamic = "force-dynamic";
 
 async function fetchData() {
-  // const response = await fetch("http://localhost:3000/api/search/seasons?season=2023-spring", {
-  const response = await fetch(`${process.env.VERCEL_URL}/api/search/seasons?season=2023-spring`, {
+  const response = await fetch("http://localhost:3000/api/search/seasons?season=2023-spring", {
+    // const response = await fetch(`${process.env.VERCEL_URL}/api/search/seasons?season=2023-spring`, {
     cache: "no-store",
   });
   return response.json();
@@ -12,6 +12,10 @@ async function fetchData() {
 
 const HomePage = async () => {
   const worksData = await fetchData();
+
+  if (!worksData) {
+    return <h1>no data</h1>;
+  }
 
   return (
     <main>
