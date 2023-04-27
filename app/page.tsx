e60a9b8f6147b766b0cdf261dc7b991738f018e6
Aaron Hsieh <110440243+ahsiehgit@users.noreply.github.com>
2023-04-27 23:47:10 +0800

    testing fetch functionality

--- app/page.tsx
+++ app/page.tsx
@@ -2,24 +2,21 @@ import MiniPage from "@/components/Works/MiniPage";
 
 export const dynamic = "force-dynamic";
 
-async function fetchData() {
-  const response = await fetch("http://localhost:3000/api/search/seasons?season=2023-spring", {
-    // const response = await fetch(`${process.env.VERCEL_URL}/api/search/seasons?season=2023-spring`, {
-    cache: "no-store",
-  });
-  return response.json();
-}
+// async function fetchData() {
+//   const response = await fetch("http://localhost:3000/api/search/seasons?season=2023-spring", {
+//     // const response = await fetch(`${process.env.VERCEL_URL}/api/search/seasons?season=2023-spring`, {
+//     cache: "no-store",
+//   });
+//   return response.json();
+// }
 
 const HomePage = async () => {
-  const worksData = await fetchData();
-
-  if (!worksData) {
-    return <h1>no data</h1>;
-  }
+  // const worksData = await fetchData();
 
   return (
     <main>
-      <MiniPage worksData={worksData} mode="view" />
+      <h1>placeholder</h1>
+      {/* <MiniPage worksData={worksData} mode="view" /> */}
     </main>
   );
 };
