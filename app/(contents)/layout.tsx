4470eb2cb28a4544e04c9c939040a5d3a4e5a31e
irigyano <110440243+irigyano@users.noreply.github.com>
2023-10-29 16:46:27 +0800

    chore: packages

--- app/(contents)/layout.tsx
+++ app/(contents)/layout.tsx
@@ -6,15 +6,25 @@ import { ReduxProvider } from "@/app/redux/ReduxProvider";
 import ReduxPreloader from "@/app/redux/ReduxPreloader";
 import { store } from "../redux/store";
 import { userAuthenticated } from "../redux/features/user/userSlice";
-import { Analytics } from "@vercel/analytics/react";
 
 export const dynamic = "force-dynamic";
 
-export default async function ContentsLayout({ children }: { children: React.ReactNode }) {
+export default async function ContentsLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   const userInfo = await getCurrentUser();
   if (userInfo) {
-    const { username, avatar, watchedWorks, watchingWorks, followingWorks } = userInfo;
-    const currentUser = { username, avatar, watchedWorks, watchingWorks, followingWorks };
+    const { username, avatar, watchedWorks, watchingWorks, followingWorks } =
+      userInfo;
+    const currentUser = {
+      username,
+      avatar,
+      watchedWorks,
+      watchingWorks,
+      followingWorks,
+    };
     store.dispatch(userAuthenticated(currentUser));
   } else {
     store.dispatch(userAuthenticated(null));
@@ -26,7 +36,6 @@ export default async function ContentsLayout({ children }: { children: React.Rea
       <ReduxProvider>
         <Navbar currentUser={store.getState().user.user} />
         {children}
-        <Analytics />
       </ReduxProvider>
     </NextAuthSessionProvider>
   );
