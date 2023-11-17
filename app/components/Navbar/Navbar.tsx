5a361bbbd776144c239e40acb6de2b402e19efe5
irigyano <110440243+irigyano@users.noreply.github.com>
2023-11-18 03:55:43 +0800

    feat: add next-themes

--- app/components/Navbar/Navbar.tsx
+++ app/components/Navbar/Navbar.tsx
@@ -2,12 +2,14 @@ import Link from "next/link";
 import Image from "next/image";
 import SearchInput from "./SearchInput";
 import MobileMenu from "./MobileMenu";
-import { BsGithub } from "react-icons/bs";
 import LoginDialog from "../LoginDialog";
 import { getServerSession } from "next-auth";
 import { authOptions } from "../../api/auth/[...nextauth]/route";
 import prisma from "@/lib/prisma";
 import ProfileDropdown from "./ProfileDropdown";
+import { ThemeToggleButton } from "./ThemeToggleButton";
+import { Github } from "lucide-react";
+import { Button } from "../ui/button";
 
 const Navbar = async () => {
   const session = await getServerSession(authOptions);
@@ -50,25 +52,25 @@ const Navbar = async () => {
         </div>
         <SearchInput />
         <div className="hidden lg:flex basis-1/3 justify-end items-center">
-          <Link
-            href={"https://github.com/irigyano/Banngumi-View"}
-            target="_blank"
-            className="hover:text-blue-500 duration-300 h-10 w-10 m-1 flex justify-center items-center"
-          >
-            <BsGithub size={36} />
-          </Link>
+          <Button variant="ghost" size="icon">
+            <Link
+              href={"https://github.com/irigyano/Banngumi-View"}
+              target="_blank"
+            >
+              <Github className="h-[1.2rem] w-[1.2rem] transition-all" />
+            </Link>
+          </Button>
+          <ThemeToggleButton />
           {currentUser ? (
             <ProfileDropdown currentUser={currentUser} />
           ) : (
             <LoginDialog>
-              <div className="h-10 w-40 flex text-center justify-center items-center">
-                <div
-                  className={
-                    "flex basis-1/2 justify-center items-center h-8 m-2 duration-300 bg-zinc-200 hover:bg-zinc-300 dark:bg-zinc-700 dark:hover:bg-zinc-500 rounded-lg shadow-md"
-                  }
-                >
-                  登入
-                </div>
+              <div
+                className={
+                  "w-20 flex basis-1/2 justify-center items-center h-8 m-2 duration-300 bg-zinc-200 hover:bg-zinc-300 dark:bg-zinc-700 dark:hover:bg-zinc-500 rounded-lg shadow-md"
+                }
+              >
+                登入
               </div>
             </LoginDialog>
           )}
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import "./globals.css";
 import { Zen_Maru_Gothic } from "next/font/google";
 import { ReduxProvider } from "@/app/redux/ReduxProvider";
 import Navbar from "@/app/components/Navbar/Navbar";
+import { ThemeProvider } from "./components/ThemeProvider";
 
 const font = Zen_Maru_Gothic({
   weight: "700",
@@ -22,15 +23,22 @@ export default function MainLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="zh-tw">
-      <body
-        className={`${font.className} min-h-screen text-[#0f0f0f] dark:text-[#f1f1f1] bg-gray-300 dark:bg-gray-800`}
-      >
-        <ReduxProvider>
-          <Navbar />
-          {children}
-        </ReduxProvider>
-      </body>
-    </html>
+    <ReduxProvider>
+      <html lang="zh-tw">
+        <body
+          className={`${font.className} min-h-screen text-[#0f0f0f] dark:text-[#f1f1f1] bg-gray-300 dark:bg-gray-800`}
+        >
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            <Navbar />
+            {children}
+          </ThemeProvider>
+        </body>
+      </html>
+    </ReduxProvider>
   );
 }
