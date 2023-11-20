c2c5778e8876cacd772b2638ec330e8cbe995d6c
irigyano <110440243+irigyano@users.noreply.github.com>
2023-11-21 00:32:05 +0800

    feat: image optimization

--- app/components/LoadingPlaceholder.tsx
+++ app/components/LoadingPlaceholder.tsx
@@ -4,7 +4,7 @@ import kyaru from "../../public/images/kyaru.gif";
 const LoadingPlaceholder = () => {
   return (
     <div className="flex justify-center items-center h-screen pb-40 w-screen fixed">
-      <Image src={kyaru} alt="Deadge" />
+      <Image src={kyaru} alt="Deadge" priority />
     </div>
   );
 };
