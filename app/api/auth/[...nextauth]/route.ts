0cefb32eb9e75d4986d918b30af9bde5131c94a6
irigyano <110440243+irigyano@users.noreply.github.com>
2024-01-25 22:30:37 +0800

    fix: next-auth authOptions type

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,29 +1,5 @@
-import NextAuth, { type NextAuthOptions } from "next-auth";
-import GithubProvider from "next-auth/providers/github";
-import { PrismaAdapter } from "@auth/prisma-adapter";
-import prisma from "@/lib/prisma";
+import { authOptions } from "@/lib/utils";
+import NextAuth from "next-auth";
 
-export const authOptions: NextAuthOptions = {
-  adapter: PrismaAdapter(prisma),
-  providers: [
-    GithubProvider({
-      clientId: process.env.GITHUB_ID!,
-      clientSecret: process.env.GITHUB_SECRET!,
-      profile(profile) {
-        return {
-          id: profile.id,
-          username: profile.login,
-          image: profile.avatar_url,
-        };
-      },
-    }),
-  ],
-  callbacks: {
-    async session({ session, user }) {
-      session.user.id = user.id;
-      return session;
-    },
-  },
-};
 const handler = NextAuth(authOptions);
 export { handler as GET, handler as POST };
