fbb0954e56fb85be7a6ae82c30fb34c5915a4c26
irigyano <110440243+irigyano@users.noreply.github.com>
2024-03-20 19:56:44 +0800

    feat: image optimization

--- app/page.tsx
+++ app/page.tsx
@@ -10,24 +10,26 @@ const LandingPage = async () => {
 
   return (
     <>
-      <div className="relative flex h-screen flex-col px-[10%]">
+      <div className="relative flex h-[100dvh] flex-col px-[10%]">
         <div className="absolute left-0 top-0 -z-30 h-full w-full">
-          <img
+          <Image
             className="h-full w-full object-cover opacity-5"
             // Yoink
+            priority
+            alt="netflix-bg"
+            fill
             src="https://assets.nflxext.com/ffe/siteui/vlv3/9d3533b2-0e2b-40b2-95e0-ecd7979cc88b/1258f4be-efa5-4738-b470-716bbd93d8ad/TW-zh-20240311-popsignuptwoweeks-perspective_alpha_website_large.jpg"
-          ></img>
+          ></Image>
         </div>
         <div className="flex flex-1 ">
-          <div className="hidden flex-1 flex-col items-center justify-center gap-4 p-4 sm:flex">
+          <div className=" relative hidden flex-1 flex-col items-center justify-center gap-4 p-4 sm:flex">
             <div className="text-4xl font-extrabold">Banngumi View</div>
             <span>將您的觀影清單在此集中管理，不再迷失於 OTT 森林之中。</span>
             <Image
-              className="fixed bottom-0 left-0 -z-30 opacity-30"
+              className="fixed bottom-0 left-0 -z-30 object-cover opacity-5"
               src={"/images/Chad.jpg"}
               alt="chad"
-              width={800}
-              height={800}
+              fill
             ></Image>
           </div>
           <div className="flex flex-1 items-center justify-center">
